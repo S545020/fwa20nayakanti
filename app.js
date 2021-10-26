95ccad30e77530301e6f458c10bb5956ae6d8357
S545020 <S545020@NWMISSOURI.EDU>
2021-10-25 19:46:39 -0500

    new end point

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var indexRouter = require('./routes/mydata');
 var app = express();
 
 // view engine setup
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/mydata',indexRouter)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
