4c68075f99136aa1cb3bf231b4d1ebfe15700ae5
S545020 <S545020@NWMISSOURI.EDU>
2021-10-27 13:39:46 -0500

    change

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var a;
+//var a;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(`Random Function applied to ${Math.random(a)} is ${Math.random(a)}`);
+  //res.send(`Random Function  is : ${Math.fround(Math.random(a))}`);
+  res.send(`Random Function  is : ${Math.random()}`);
 });
 
 module.exports = router;
