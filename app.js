14693a954356a2d738843a15dc28fd3e0302ca4a
S545020 <S545020@NWMISSOURI.EDU>
2021-10-27 11:06:39 -0500

    added bonus files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var indexRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/computation');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/computation', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/computation.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var a;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(`Random Function: ${Math.random(a).random}`);
+});
+
+module.exports = router;
